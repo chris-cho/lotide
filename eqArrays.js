480464c200c3cbc54f258d96e191f238bec45003
Chris <hyunsangc@gmail.com>
2021-08-14 16:11:04 +0000

    adding eqArrays test

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrayOne, arrayTwo) {
   let index = 0;
@@ -22,4 +16,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
