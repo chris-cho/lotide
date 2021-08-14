e58b947b7d8e4d6ee148111c6fc3dd0bb50b8b66
Chris <hyunsangc@gmail.com>
2021-08-14 16:46:13 +0000

    adding mocha and chai to tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,20 @@
-const assertEqual = require('../assertEqual');
+/* const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail! */
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns ['New', 'Lighthouse', 'Labs'] for ['Hello', 'New', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "New", "Lighthouse", "Labs"]), ["New", "Lighthouse", "Labs"]);
+  });
+
+});
