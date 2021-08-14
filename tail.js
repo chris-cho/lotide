aad48f568fde94a1f87479c703b154dfde33bb01
Chris <hyunsangc@gmail.com>
2021-08-14 16:08:23 +0000

    adding tail test files

--- tail.js
+++ tail.js
@@ -1,17 +1,8 @@
-//from assertEqual.js
-const assertEqual = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 //starts here!
 const tail = function(someArray) {
   return someArray.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+module.exports = tail;
