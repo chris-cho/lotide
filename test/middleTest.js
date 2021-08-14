58a4d97a5adc85192aeac54fcbc1493ecfc8fabd
Chris <hyunsangc@gmail.com>
2021-08-14 16:56:19 +0000

    adding _ functionality to all lotide tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const _ = require('../index');
 
-assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
+describe("#middle", () => {
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(_.middle([1, 2, 3]),[2]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(_.middle([1, 2, 3, 4, 5, 6]),[3,4]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,14 @@
-/* const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail! */
-
 const assert = require('chai').assert;
-const tail = require('../tail');
+const _ = require('../index');
 
 describe("#tail", () => {
 
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    assert.deepEqual(_.tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
   it("returns ['New', 'Lighthouse', 'Labs'] for ['Hello', 'New', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "New", "Lighthouse", "Labs"]), ["New", "Lighthouse", "Labs"]);
+    assert.deepEqual(_.tail(["Hello", "New", "Lighthouse", "Labs"]), ["New", "Lighthouse", "Labs"]);
   });
 
 });
