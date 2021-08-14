ebd1c9fc8f1956c50f9448dce50c707309c3e02c
Chris <hyunsangc@gmail.com>
2021-08-14 16:04:44 +0000

    pushing latest assert update on lotide

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
-//from assertEqual.js
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 //starts here!
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head("s"), "Hi");
+module.exports = head;
